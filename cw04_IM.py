3602523bebfe464cdedbb3b839198517a50469d0
ro2bi <152693157+ro2bi@users.noreply.github.com>
2024-11-10 20:36:48 +0200

    Add files via upload

---
+++ cw04_IM.py
@@ -0,0 +1,17 @@
+from string import ascii_lowercase as alphabet
+
+#name = "mike"
+#email = "mike@gmail.com"
+
+#msg = "user " + name + " with email " + email + " created"
+#print (msg)
+
+#msg2 = f"user {name} with email {email} created"
+#print(msg2)
+
+#print (f"{name=}")
+
+mystr = "qwerty"
+
+for symbol in mystr:
+    print(symbol)
